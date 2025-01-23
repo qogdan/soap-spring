05ecd04425c755f3b7c57df23abdf4cc56e2ae9a
Bogdan Petrovic <bogdanxpetrovic@gmail.com>
2025-01-23 12:25:36 +0100

    create file

--- src/main/java/com/webservice/soap/CountryRepository.java
+++ src/main/java/com/webservice/soap/CountryRepository.java
@@ -1,4 +1,10 @@
-package com.webservice.soap;
-
-public class CountryRepository {
-}
+//package com.webservice.soap;
+//
+//import org.springframework.stereotype.Component;
+//
+//import java.util.HashMap;
+//
+//@Component
+//public class CountryRepository {
+//        private static final Map<String, Country> countries = new HashMap<>();
+//}
